458e3d410705b433dd6b612929aa25f1b3b198d9
listlessbird <124798751+listlessbird@users.noreply.github.com>
2023-11-30 18:12:14 +0530

    Try to load three from user opts if not in window

--- src/vanta.dots.js
+++ src/vanta.dots.js
@@ -1,7 +1,7 @@
-import VantaBase, {VANTA} from './_base.js'
-import {rn, ri, sample, mobileCheck} from './helpers.js'
+import VantaBase, { VANTA } from "./_base.js"
+import { rn, ri, sample, mobileCheck } from "./helpers.js"
 
-const win = typeof window == 'object'
+const win = typeof window === "object"
 let THREE = win && window.THREE
 
 class Effect extends VantaBase {
@@ -13,62 +13,78 @@ class Effect extends VantaBase {
       size: 3,
       spacing: 35,
       showLines: true,
-    };
+    }
   }
 
   onInit() {
-    var camera = this.camera = new THREE.PerspectiveCamera(50, this.width / this.height, 0.1, 5000)
+    if (!THREE) {
+      //  a band aid for SSR
+      // try to get the THREE object from user options if it's not in the window
+      console.log(
+        "%cVANTA: %cThree.js not found on window, attempting to load from user options",
+        "color: #ff8820; font-weight: bold;",
+        "color: white; font-weight: bold;"
+      )
+
+      THREE = this.options.THREE || THREE
+    }
+
+    var camera = (this.camera = new THREE.PerspectiveCamera(
+      50,
+      this.width / this.height,
+      0.1,
+      5000
+    ))
     camera.position.x = 0
     camera.position.y = 250
     camera.position.z = 50
     camera.tx = 0
     camera.ty = 50
     camera.tz = 350
-    camera.lookAt(0,0,0)
+    camera.lookAt(0, 0, 0)
     this.scene.add(camera)
 
-    var starsGeometry = this.starsGeometry = new THREE.BufferGeometry()
-    var i,j,k,l,star,starsMaterial,starField
+    var starsGeometry = (this.starsGeometry = new THREE.BufferGeometry())
+    var i, j, k, l, star, starsMaterial, starField
     var space = this.options.spacing
     const points = []
 
     for (i = k = -30; k <= 30; i = ++k) {
       for (j = l = -30; l <= 30; j = ++l) {
         star = new THREE.Vector3()
-        star.x = i * space + space/2
+        star.x = i * space + space / 2
         star.y = rn(0, 5) - 150
-        star.z = j * space + space/2
+        star.z = j * space + space / 2
         points.push(star)
       }
     }
     starsGeometry.setFromPoints(points)
 
-
     starsMaterial = new THREE.PointsMaterial({
       color: this.options.color,
-      size: this.options.size
-    });
+      size: this.options.size,
+    })
     starField = this.starField = new THREE.Points(starsGeometry, starsMaterial)
     this.scene.add(starField)
 
     if (this.options.showLines) {
-      var material = new THREE.LineBasicMaterial( { color: this.options.color2 } );
+      var material = new THREE.LineBasicMaterial({ color: this.options.color2 })
       var linesGeo = new THREE.BufferGeometry()
       const points = []
-      for (i = 0; i < 200; i ++) {
-        var f1 = rn(40,60)
-        var f2 = f1 + rn(12,20)
+      for (i = 0; i < 200; i++) {
+        var f1 = rn(40, 60)
+        var f2 = f1 + rn(12, 20)
         // https://math.stackexchange.com/questions/1585975/how-to-generate-random-points-on-a-sphere
-        var z = rn(-1,1)
-        var r = Math.sqrt(1 - z*z)
+        var z = rn(-1, 1)
+        var r = Math.sqrt(1 - z * z)
         var theta = rn(0, Math.PI * 2)
         var y = Math.sin(theta) * r
         var x = Math.cos(theta) * r
-        points.push(new THREE.Vector3( x*f1, y*f1, z*f1) )
-        points.push(new THREE.Vector3( x*f2, y*f2, z*f2) )
+        points.push(new THREE.Vector3(x * f1, y * f1, z * f1))
+        points.push(new THREE.Vector3(x * f2, y * f2, z * f2))
       }
       linesGeo.setFromPoints(points)
-      this.linesMesh = new THREE.LineSegments( linesGeo, material )
+      this.linesMesh = new THREE.LineSegments(linesGeo, material)
       this.scene.add(this.linesMesh)
     }
 
@@ -92,13 +108,17 @@ class Effect extends VantaBase {
   onUpdate() {
     const starsGeometry = this.starsGeometry
     const starField = this.starField
-    for (var j = 0; j < starsGeometry.attributes.position.array.length; j+=3) {
+    for (
+      var j = 0;
+      j < starsGeometry.attributes.position.array.length;
+      j += 3
+    ) {
       const x = starsGeometry.attributes.position.array[j]
-      const y = starsGeometry.attributes.position.array[j+1]
-      const z = starsGeometry.attributes.position.array[j+2]
+      const y = starsGeometry.attributes.position.array[j + 1]
+      const z = starsGeometry.attributes.position.array[j + 2]
       // var i = starsGeometry.vertices[j]
-      const newY = y + 0.1 * Math.sin(z*0.02 + x*0.015 + this.t*0.02)
-      starsGeometry.attributes.position.array[j+1] = newY
+      const newY = y + 0.1 * Math.sin(z * 0.02 + x * 0.015 + this.t * 0.02)
+      starsGeometry.attributes.position.array[j + 1] = newY
     }
 
     starsGeometry.attributes.position.setUsage(THREE.DynamicDrawUsage)
@@ -110,7 +130,7 @@ class Effect extends VantaBase {
     c.position.x += (c.tx - c.position.x) * rate
     c.position.y += (c.ty - c.position.y) * rate
     c.position.z += (c.tz - c.position.z) * rate
-    c.lookAt(0,0,0)
+    c.lookAt(0, 0, 0)
 
     if (this.linesMesh) {
       this.linesMesh.rotation.z += 0.002
@@ -120,14 +140,14 @@ class Effect extends VantaBase {
     }
   }
 
-  onMouseMove(x,y) {
+  onMouseMove(x, y) {
     this.camera.tx = (x - 0.5) * 100 // -50 to 50
     this.camera.ty = 50 + y * 50 // 50 to 100
   }
 
   onRestart() {
-    this.scene.remove( this.starField )
+    this.scene.remove(this.starField)
   }
 }
 Effect.initClass()
-export default VANTA.register('DOTS', Effect)
+export default VANTA.register("DOTS", Effect)
